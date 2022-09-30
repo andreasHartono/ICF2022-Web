70f8336ddc8cacb28e772e56c59a0510d10ff3b2
andreas <s160419047@student.ubaya.ac.id>
2022-09-30 12:52:42 +0700

    update admin welcome

--- resources/views/admin/adminwelcome.blade.php
+++ resources/views/admin/adminwelcome.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.admin')
 @section('header')
    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-      <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
-      <li class="breadcrumb-item text-sm text-white active" aria-current="page">Tables</li>
+      <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Admin</a></li>
+      <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
    </ol>
-   <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+   <h6 class="font-weight-bolder text-white mb-0">Welcome Admin</h6>
 @endsection
 @section('content')
-   
+   <div class="col-lg-12">
+            <!-- START card -->
+            <div class="card bg-gradient-danger text-white">
+                <div class="card-header">
+                    <div class="card-title">
+                        <h1>Welcome to Admin Dashboard ICF 2022</h1>
+                    </div>
+                    {{-- <div class="clearfix"></div> --}}
+                </div>
+                <div class="card-body">
+                    <p>
+                       Selamat Datang di Admin Dashboard ICF 2022. Untuk validasi data  data dapat anda
+                        lakukan dengan memilih menu pada bagian kiri.<br /> Selamat Bertugas .Terima Kasih.
+                     </p>
+                </div>
+            </div>
+            <!-- END card -->
+        </div>
 @endsection

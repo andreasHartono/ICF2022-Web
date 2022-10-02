76da97e7cdd37f8d9ee731c5fb837b196c90aa13
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2022-10-02 20:55:08 +0700

    Navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,132 +1,26 @@
+<link rel="stylesheet" href="{{ asset('assets/css/navbar.css') }}">
 <div class="content-wrapper">
     <header class="wrapper bg-soft-primary">
-        <nav class="navbar navbar-expand-lg center-nav transparent navbar-light">
+        <nav class="navbar navbar-expand-lg center-nav">
             <div class="container flex-lg-row flex-nowrap align-items-center">
                 <div class="navbar-brand w-100">
                     <a href="#">
-                        <img src="{{ asset('assets/img/logo/logoUtama.png') }}" srcset="{{ asset('assets/img/logo/logoUtama.png') }}" alt=""  style="height: 90px; width: 170px"/>
+                        <img class="logo" src="{{ asset('assets/img/logo/logoUtama.png') }}"
+                            srcset="{{ asset('assets/img/logo/logoUtama.png') }}" alt="" />
                     </a>
                 </div>
                 <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
-                    <div class="offcanvas-header d-lg-none">
-                        <h3 class="text-white fs-30 mb-0">Sandbox</h3>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                            aria-label="Close"></button>
-                    </div>
                     <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
                         <ul class="navbar-nav">
-                            <li class="nav-item dropdown dropdown-mega">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Home</a>
-                                <ul class="dropdown-menu mega-menu mega-menu-dark mega-menu-img">
-                                    <li class="mega-menu-content mega-menu-scroll">
-                                        <ul class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-4 gy-lg-2 list-unstyled">
-                                            <li class="col"><a class="dropdown-item" href="#">
-                                                    <figure class="rounded lift d-none d-lg-block"><img
-                                                            src="{{ asset('assets/img/demos/mi1.jpg') }}"
-                                                            srcset="{{ asset('assets/img/demos/mi1@2x.jpg 2x') }}./"
-                                                            alt=""></figure>
-                                                    <span class="d-lg-none">Demo I</span>
-                                                </a></li>
-                                            <li class="col"><a class="dropdown-item" href="#">
-                                                    <figure class="rounded lift d-none d-lg-block"><img
-                                                            src="{{ asset('assets/img/demos/mi2.jpg') }}"
-                                                            srcset="{{ asset('assets/img/demos/mi2@2x.jpg 2x') }}"
-                                                            alt=""></figure>
-                                                    <span class="d-lg-none">Demo II</span>
-                                                </a></li>
-                                        </ul>
-                                        <!--/.row -->
-                                        <span class="d-none d-lg-flex"><i class="uil uil-direction"></i><strong>Scroll
-                                                to view more</strong></span>
-                                    </li>
-                                    <!--/.mega-menu-content-->
-                                </ul>
-                                <!--/.dropdown-menu -->
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#"
-                                    data-bs-toggle="dropdown">Competition</a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown dropdown-submenu dropend"><a
-                                            class="dropdown-item dropdown-toggle" href="#"
-                                            data-bs-toggle="dropdown">Services</a>
-                                        <ul class="dropdown-menu">
-                                            <li class="nav-item"><a class="dropdown-item" href="#">Services I</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown dropdown-submenu dropend"><a
-                                            class="dropdown-item dropdown-toggle" href="#"
-                                            data-bs-toggle="dropdown">About</a>
-                                        <ul class="dropdown-menu">
-                                            <li class="nav-item"><a class="dropdown-item" href="#">About I</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#"
-                                    data-bs-toggle="dropdown">Workshop</a>
-                                <div class="dropdown-menu dropdown-lg">
-                                    <div class="dropdown-lg-content">
-                                        <div>
-                                            <h6 class="dropdown-header">Project Pages</h6>
-                                            <ul class="list-unstyled">
-                                                <li><a class="dropdown-item" href="#">Projects I</a></li>
-                                            </ul>
-                                        </div>
-                                        <!-- /.column -->
-                                        <div>
-                                            <h6 class="dropdown-header">Single Projects</h6>
-                                            <ul class="list-unstyled">
-                                                <li><a class="dropdown-item" href="#">Single Project I</a></li>
-                                            </ul>
-                                        </div>
-                                        <!-- /.column -->
-                                    </div>
-                                    <!-- /auto-column -->
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">About
-                                    Us</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item"><a class="dropdown-item" href="#">Blog without
-                                            Sidebar</a></li>
-
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown dropdown-mega">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Blocks</a>
-                                <ul class="dropdown-menu mega-menu mega-menu-dark mega-menu-img">
-                                    <li class="mega-menu-content">
-                                        <ul class="row row-cols-1 row-cols-lg-6 gx-0 gx-lg-6 gy-lg-4 list-unstyled">
-                                            <li class="col"><a class="dropdown-item" href="./docs/block#">
-                                                    <div class="rounded img-svg d-none d-lg-block p-4 mb-lg-2"><img
-                                                            class="rounded-0"
-                                                            src="{{ asset('assets/img/demos/block1.svg') }}"
-                                                            alt=""></div>
-                                                    <span>About</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                        <!--/.row -->
-                                    </li>
-                                    <!--/.mega-menu-content-->
-                                </ul>
-                                <!--/.dropdown-menu -->
-                            </li>
-
-                            <!--/.mega-menu-content-->
-                        </ul>
-                        <!--/.dropdown-menu -->
-                        </li>
+                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link " href="#">Competition</a>
+                            <a class="nav-link " href="#">Workshop</a>
+                            <a class="nav-link " href="#">About Us</a>
                         </ul>
                         <!-- /.navbar-nav -->
                         <div class="offcanvas-footer d-lg-none">
                             <div>
-                                <a href="mailto:first.last@email.com" class="link-inverse">info@email.com</a>
+                              <p>Contact Person</p>
                                 <br /> 00 (123) 456 78 90 <br />
                                 <nav class="nav social social-white mt-4">
                                     <a href="#"><i class="uil uil-twitter"></i></a>
@@ -145,16 +39,9 @@
                 <!-- /.navbar-collapse -->
                 <div class="navbar-other w-100 d-flex ms-auto">
                     <ul class="navbar-nav flex-row align-items-center ms-auto">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link" data-bs-toggle="modal"
-                                data-bs-target="#modal-signin">LOGIN</a>
-                        </li>
                         <li class="nav-item d-none d-md-block">
-                            <a href="#" class="btn btn-sm btn-primary rounded" data-bs-toggle="modal"
-                                data-bs-target="#modal-signup">Sign Up</a>
-                        </li>
-                        <li class="nav-item d-lg-none">
-                            <button class="hamburger offcanvas-nav-btn"><span></span></button>
+                            <a href="#" class="btn btn-sm btn-primary rounded">Login</a>
+                            <a href="#" class="btn btn-sm btn-primary rounded">Sign Up</a>
                         </li>
                     </ul>
                     <!-- /.navbar-nav -->
@@ -163,101 +50,6 @@
             </div>
             <!-- /.container -->
         </nav>
-        <!-- /.navbar -->
-        <div class="modal fade" id="modal-signin" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered modal-sm">
-                <div class="modal-content text-center">
-                    <div class="modal-body">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                        <h2 class="mb-3 text-start">Welcome Back</h2>
-                        <p class="lead mb-6 text-start">Fill your email and password to sign in.</p>
-                        <form class="text-start mb-3">
-                            <div class="form-floating mb-4">
-                                <input type="email" class="form-control" placeholder="Email" id="loginEmail">
-                                <label for="loginEmail">Email</label>
-                            </div>
-                            <div class="form-floating password-field mb-4">
-                                <input type="password" class="form-control" placeholder="Password"
-                                    id="loginPassword">
-                                <span class="password-toggle"><i class="uil uil-eye"></i></span>
-                                <label for="loginPassword">Password</label>
-                            </div>
-                            <a class="btn btn-primary rounded-pill btn-login w-100 mb-2">Sign In</a>
-                        </form>
-                        <!-- /form -->
-                        <p class="mb-1"><a href="#" class="hover">Forgot Password?</a></p>
-                        <p class="mb-0">Don't have an account? <a href="#" data-bs-target="#modal-signup"
-                                data-bs-toggle="modal" data-bs-dismiss="modal" class="hover">Sign up</a></p>
-                        <div class="divider-icon my-4">or</div>
-                        <nav class="nav social justify-content-center text-center">
-                            <a href="#" class="btn btn-circle btn-sm btn-google"><i
-                                    class="uil uil-google"></i></a>
-                            <a href="#" class="btn btn-circle btn-sm btn-facebook-f"><i
-                                    class="uil uil-facebook-f"></i></a>
-                            <a href="#" class="btn btn-circle btn-sm btn-twitter"><i
-                                    class="uil uil-twitter"></i></a>
-                        </nav>
-                        <!--/.social -->
-                    </div>
-                    <!--/.modal-content -->
-                </div>
-                <!--/.modal-body -->
-            </div>
-            <!--/.modal-dialog -->
-        </div>
-        <!--/.modal -->
-        <div class="modal fade" id="modal-signup" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered modal-sm">
-                <div class="modal-content text-center">
-                    <div class="modal-body">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                        <h2 class="mb-3 text-start">Sign up to Sandbox</h2>
-                        <p class="lead mb-6 text-start">Registration takes less than a minute.</p>
-                        <form class="text-start mb-3">
-                            <div class="form-floating mb-4">
-                                <input type="text" class="form-control" placeholder="Name" id="loginName">
-                                <label for="loginName">Name</label>
-                            </div>
-                            <div class="form-floating mb-4">
-                                <input type="email" class="form-control" placeholder="Email" id="loginEmail">
-                                <label for="loginEmail">Email</label>
-                            </div>
-                            <div class="form-floating password-field mb-4">
-                                <input type="password" class="form-control" placeholder="Password"
-                                    id="loginPassword">
-                                <span class="password-toggle"><i class="uil uil-eye"></i></span>
-                                <label for="loginPassword">Password</label>
-                            </div>
-                            <div class="form-floating password-field mb-4">
-                                <input type="password" class="form-control" placeholder="Confirm Password"
-                                    id="loginPasswordConfirm">
-                                <span class="password-toggle"><i class="uil uil-eye"></i></span>
-                                <label for="loginPasswordConfirm">Confirm Password</label>
-                            </div>
-                            <a class="btn btn-primary rounded-pill btn-login w-100 mb-2">Sign Up</a>
-                        </form>
-                        <!-- /form -->
-                        <p class="mb-0">Already have an account? <a href="#" data-bs-target="#modal-signin"
-                                data-bs-toggle="modal" data-bs-dismiss="modal" class="hover">Sign in</a></p>
-                        <div class="divider-icon my-4">or</div>
-                        <nav class="nav social justify-content-center text-center">
-                            <a href="#" class="btn btn-circle btn-sm btn-google"><i
-                                    class="uil uil-google"></i></a>
-                            <a href="#" class="btn btn-circle btn-sm btn-facebook-f"><i
-                                    class="uil uil-facebook-f"></i></a>
-                            <a href="#" class="btn btn-circle btn-sm btn-twitter"><i
-                                    class="uil uil-twitter"></i></a>
-                        </nav>
-                        <!--/.social -->
-                    </div>
-                    <!--/.modal-content -->
-                </div>
-                <!--/.modal-body -->
-            </div>
-            <!--/.modal-dialog -->
-        </div>
-        <!--/.modal -->
     </header>
     <!-- /header -->
+</div>

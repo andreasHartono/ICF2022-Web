5cf9573b4e7f1ffd3621cd31a7ba49fff30603aa
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2022-10-11 11:00:02 +0700

    perbaiki gk bs alogin

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -134,7 +134,7 @@
                         </li>
                     @endauth
                     @else
-                    <li class="nav-item">
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <li><a class="dropdown-item" href="{{ url('/login') }}">Login</a></li>

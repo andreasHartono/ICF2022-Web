7d9904901f83bebe98d4df6a33847aea3c0308b3
andreas <s160419047@student.ubaya.ac.id>
2022-10-12 20:06:22 +0700

    rapihkan halaman utama

--- resources/views/workshop.blade.php
+++ resources/views/workshop.blade.php
@@ -5,30 +5,29 @@ ICF 2022 - Workshop
 @endsection
 
 @section('content')
-@for ($i=0; $i<count($workshop); $i++)
-    <div class="d-flex justify-content-center" data-cues="slideInDown" data-group="page-title-buttons" data-delay="600" >
+@foreach ($workshop as $k => $w)
+   <div class="d-flex justify-content-center" data-cues="slideInDown" data-group="page-title-buttons" data-delay="600" >
         <div class="workshop-container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6 col-sm-12 mb-3 d-flex justify-content-center workshop-poster">
-                    <img src="{{ url('/assets/img/poster.png') }}" alt="Poster Workshop {{ $workshop[$i]->url }}" style="width: 80%;">
+                    <img src="{{ url('/assets/img/poster.png') }}" alt="Poster Workshop {{ $w->url }}" style="width: 80%;">
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 mb-7 workshop-regis">
                     <div class="card mx-auto workshop-card" style="">
-                        <h4>{{ $workshop[$i]->nama }}</h4>
-                        <p style="text-align: justify;">{{ $workshop[$i]->deskripsi }}</p>
-                        <p>Lokasi Acara : {{ $workshop[$i]->lokasi }}</p>
-                        <p style="text-align: justify;">Syarat & Ketentuan <br> {{ $workshop[$i]->term_condition }}</p>
+                        <h3 class="text-center">{{ $w->nama }}</h3>
                         <ul>
-                            @php $date1 = date_create($workshop[$i]->tanggal_start);  $date2 = date_create($workshop[$i]->tanggal_end); @endphp
-                            <li>Tanggal: {{ date_format($date1, "d F Y") }}<br>Pukul : {{ date_format($date1, "H:i")}} - {{ date_format($date2, "H:i")}}</li>
+                           <li style="list-style: none;"><p style="color: black;">{{ $w->deskripsi }}</p></li>
+                            <li style="list-style: none;"><p style="text-align: justify;"><h6>Lokasi Acara</h6> {{ $w->lokasi }}</p></li>
+                            @php $date1 = date_create($w->tanggal_start);  $date2 = date_create($w->tanggal_end); @endphp
+                            <li style="list-style: none;"><h6>Tanggal Acara</h6>{{ date_format($date1, "d F Y") }}<br>Pukul : {{ date_format($date1, "H:i")}} - {{ date_format($date2, "H:i")}}</li>
+                            <li style="list-style: none;"><p style="text-align: justify; color: black;"><h6>Syarat & Ketentuan</h6> {{ $w->term_condition }}</p></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-@endfor
-
+@endforeach
 </div>
 </div>
 @endsection

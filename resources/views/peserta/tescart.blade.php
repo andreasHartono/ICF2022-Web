dc7e91ad874b3b6fc838d5856674e9b5bd319247
andreas <s160419047@student.ubaya.ac.id>
2022-10-13 00:23:04 +0700

    update team detail di halaman peserta

---
+++ resources/views/peserta/tescart.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.mainweb')
+
+@section('title')
+    Dashboard
+@endsection
+
+{{-- @section('style')
+    <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
+@endsection --}}
+
+@section('content')
+
+
+@endsection
